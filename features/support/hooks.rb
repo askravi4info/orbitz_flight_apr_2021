65852d4c9b0688539ae962554d8a74fd51e611b3
askravi <askravi4info@gmail.com>
2021-06-14 20:35:43 -0400

    Added the env variables

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,10 +2,16 @@ require 'watir'
 
 Before do |scenario|
   DataMagic.load_for_scenario(scenario)
-  @browser = Watir::Browser.new :chrome
+  @browser = Watir::Browser.new $browser_type
 end
 
 
 After do
   @browser.close
+end
+
+After do |scenario|
+  if scenario.failed?
+    @browser.screenshot("failed.jpg")
+  end
 end
