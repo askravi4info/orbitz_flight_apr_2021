331ed5ad515494ec4073a31949f03d46ee98ecfe
askravi <askravi4info@gmail.com>
2021-06-16 20:57:34 -0400

    Added logic for screenshot and updated cucumber.yml

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -9,6 +9,13 @@ After do
   @browser.close
 end
 
-After do
-
+After do |scenario|
+  SCREENSHOTS_DIRECTORY = "html-report"
+  if scenario.failed?
+    #Saves screenshots to features/test_results/Screenshots directory, name is scenario name + timestamp
+    time_stamp = Time.now.to_s.gsub('-', '_').gsub(" ","_").gsub(':','_').gsub(/__0400/,'')
+    screenshot = "#{scenario.name.gsub(' ', '_').gsub(/[^0-9A-Za-z_]/, '')}_#{time_stamp}.png"
+    @browser.driver.save_screenshot(File.join(SCREENSHOTS_DIRECTORY, screenshot))
+    embed(File.join(SCREENSHOTS_DIRECTORY, screenshot), "image/png", "SCREENSHOT")
+  end
 end
