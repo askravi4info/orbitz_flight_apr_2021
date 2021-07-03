6c4a8b15fe44420a66b7b285c97be281499cd0a6
askravi <askravi4info@gmail.com>
2021-07-03 00:30:34 -0400

    Added More ways of API tests

--- api_tests.rb
+++ api_tests.rb
@@ -1,14 +1,37 @@
-require 'httparty'
+require 'rest-client'
+require 'json'
 
 url = 'https://jsonplaceholder.typicode.com/users'
-response = HTTParty.get(url)
+response = RestClient.get(url)
 puts "The response is: #{response}"
 puts "+++++++++++++++++++++"
-value = response.parsed_response
+value = JSON.parse(response)
 puts "value is #{value}"
 fail "#{value[0]["name"]} is NOT correct." unless value[0]["name"] == 'Leanne Graham'
 
-
-
-
-
+# # *************************
+#
+# require 'net/http'
+# require 'json'
+#
+# url = 'https://jsonplaceholder.typicode.com/users'
+# uri = URI(url)
+# response = Net::HTTP.get(uri)
+# puts "The response is: #{response}"
+# puts "+++++++++++++++++++++"
+# value = JSON.parse(response)
+# puts "value is #{value}"
+# fail "#{value[0]["name"]} is NOT correct." unless value[0]["name"] == 'Leanne Graham'
+#
+#
+# # *************************
+#
+# require 'httparty'
+#
+# url = 'https://jsonplaceholder.typicode.com/users'
+# response = HTTParty.get(url)
+# puts "The response is: #{response}"
+# puts "+++++++++++++++++++++"
+# value = response.parsed_response
+# puts "value is #{value}"
+# fail "#{value[0]["name"]} is NOT correct." unless value[0]["name"] == 'Leanne Graham'
